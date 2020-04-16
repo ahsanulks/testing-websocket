79e243c8f28edf52db6ae2d4505fe508f4ab7260
Ahsan <ahsanulkh996@gmail.com>
2020-04-16 14:57:18 +0700

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/ahsanulks/testing-websocket
+
+go 1.13
+
+require (
+	github.com/gofiber/fiber v1.8.33
+	github.com/gofiber/websocket v0.0.1
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/savsgio/gotils v0.0.0-20200319105752-a9cc718f6a3f // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/websocket"
+)
+
+type MessagePayload struct {
+	Username string `json:"Username"`
+	Type     string `json:"Type"`
+	Message  string `json:"Message"`
+}
+
+type Conn struct {
+	Connection *websocket.Conn
+	Username   string
+}
+
+var connections []*Conn
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) {
+		// c.Accepts("text/html")
+		data := make(map[string]interface{})
+		c.Render("./view/index2.html", data)
+	})
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		mt, msg, _ := c.ReadMessage()
+		var message MessagePayload
+		json.Unmarshal(msg, &message)
+		currentConnection := Conn{
+			Connection: c,
+			Username:   message.Username,
+		}
+		connections = append(connections, &currentConnection)
+		messageSend := MessagePayload{
+			Username: message.Username,
+			Type:     "New",
+			Message:  "joining the chat",
+		}
+		jsonMessage, _ := json.Marshal(messageSend)
+		broardcast(&currentConnection, mt, jsonMessage)
+		handleConnection(&currentConnection)
+	}))
+
+	app.Listen(3000)
+}
+
+func handleConnection(currentConnection *Conn) {
+	for {
+		mt, msg, err := currentConnection.Connection.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			removeConnection(currentConnection)
+			messageSend := MessagePayload{
+				Username: currentConnection.Username,
+				Type:     "Leave",
+				Message:  "Leave the chat",
+			}
+			jsonMessage, _ := json.Marshal(messageSend)
+			broardcast(currentConnection, 1, jsonMessage)
+			break
+		}
+		var a MessagePayload
+		json.Unmarshal(msg, &a)
+		if a.Type == "message" {
+			a.Type = "message"
+			jsonMessage, _ := json.Marshal(a)
+			broardcast(currentConnection, mt, jsonMessage)
+		} else {
+			jsonMessage, _ := json.Marshal(a.Username + " leave the chat")
+			broardcast(currentConnection, mt, jsonMessage)
+		}
+		// log.Printf("recv: %s", msg)
+		// err = currentConnection.WriteMessage(mt, msg)
+		// if err != nil {
+		// 	log.Println("write:", err)
+		// 	break
+		// }
+	}
+}
+
+func broardcast(currentConnection *Conn, messageType int, message []byte) {
+	// fmt.Println(connections)
+	connects := connections
+	for _, v := range connects {
+		if v == currentConnection {
+			continue
+		}
+		v.Connection.WriteMessage(messageType, message)
+	}
+}
+
+func removeConnection(currentConnection *Conn) {
+	var filtered []*Conn
+	for _, v := range connections {
+		if v != currentConnection {
+			filtered = append(filtered, v)
+		}
+	}
+	connections = filtered
+}
